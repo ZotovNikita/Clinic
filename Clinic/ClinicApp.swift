553d9a9b0fab0a6783d87e9ede619c03c8ff7ac4
Nikita <usernamex821@gmail.com>
2023-11-21 08:42:58 -0800

    Initial Commit

---
+++ Clinic/ClinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicApp.swift
+//  Clinic
+//
+//  Created by Никита on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ClinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
